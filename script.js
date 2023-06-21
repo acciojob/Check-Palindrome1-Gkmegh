8f73fc16724ccccb78378efd8811d01f651bc206
Gkmegh <118533859+Gkmegh@users.noreply.github.com>
2023-06-21 10:04:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
-function palindrome(str){
-    let n = str.length();
-	str = str.toLowerCase();
-	let start =0;
-	let end = n-1;
-	while(start <= end){
-	    if(str(start) !== str(end)){
-	        return false;
-	    }
-	    start++;
-	    end--;
-	}
-	return true;
-
+function palindrome(str) {
+    let n = str.length;
+    str = str.toLowerCase();
+    let start = 0;
+    let end = n - 1;
+    while (start <= end) {
+        if (str[start] !== str[end]) {
+            return false;
+        }
+        start++;
+        end--;
+    }
+    return true;
 }
-module.exports = palindrome
+module.exports = palindrome;

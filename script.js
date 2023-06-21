fc9be8a77dbe6c588fb1d98b16b41c7f783b1b7f
Gkmegh <118533859+Gkmegh@users.noreply.github.com>
2023-06-21 09:56:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
-// complete the given function
-
 function palindrome(str){
+    let n = str.length();
+	str = str.toLowerCase();
+	let start =0;
+	let end = n-1;
+	while(start <= end){
+	    if(str(start) !== str(end)){
+	        return false;
+	    }
+	    start++;
+	    end--;
+	}
+	return true;
 
 }
 module.exports = palindrome
